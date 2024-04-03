31f7faef5aff3312eed29dfcb988915e2490f5f9
Karan samrat <karansamrat05039@gmail.com>
2024-04-03 13:15:53 +0530

    chnages done in galgotiya

--- cityedu_galgotiya/src/Components/Header/Header.jsx
+++ cityedu_galgotiya/src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -7,7 +8,9 @@ const Header = () => {
       <div class="row main-heder-of-city-guidde ">
         <div class="col-12 heder-of-city-guidde">
           <img src={require("../../Assets/galogotiy.png")} alt="" />
-          <span style={{ color: "white" }}>For Info : (+91) 9427 700 700</span>
+          <span style={{ color: "white" }}>
+            <FaPhoneAlt /> (+91) 9427 700 700
+          </span>
         </div>
       </div>
     </>
